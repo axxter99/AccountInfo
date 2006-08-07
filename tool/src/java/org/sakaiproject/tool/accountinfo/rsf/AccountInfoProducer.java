a79712bcb590ca71bd10b6584c498622bedac199
David Horwitz <dhorwitz@gmail.com>
2006-08-07 11:02:31 +0000

    added copyright notice

--- tool/src/java/org/sakaiproject/tool/accountinfo/rsf/AccountInfoProducer.java
+++ tool/src/java/org/sakaiproject/tool/accountinfo/rsf/AccountInfoProducer.java
@@ -2,6 +2,26 @@
  * Created on May 29, 2006
  */
 package org.sakaiproject.tool.accountinfo.rsf;
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The University of Cape Town.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
 import java.text.DateFormat;
 import java.util.Date;
