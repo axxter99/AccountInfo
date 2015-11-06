c2ca0881e05f0f74278fa6d78779fd3cdf2497e6
David Horwitz <dhorwitz@gmail.com>
2015-11-06 15:31:39 +0000

    NOJIRA Format, List<NavigationCase>

--- tool/src/java/org/sakaiproject/tool/accountinfo/rsf/AccountInfoProducer.java
+++ tool/src/java/org/sakaiproject/tool/accountinfo/rsf/AccountInfoProducer.java
@@ -49,114 +49,111 @@ import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
+public class AccountInfoProducer implements ViewComponentProducer, NavigationCaseReporter, DefaultView {
+	public static final String VIEW_ID = "AccountInfo";
+	private UserDirectoryService userDirectoryService;
+	private MessageLocator messageLocator;
+	private LocaleGetter localegetter;
 
-public class AccountInfoProducer implements ViewComponentProducer,
-    NavigationCaseReporter, DefaultView {
-  public static final String VIEW_ID = "AccountInfo";
-  private UserDirectoryService userDirectoryService;
-  private MessageLocator messageLocator;
-  private LocaleGetter localegetter;
-  
-  private long CACHETTL = 300000;
-  
-  
-  private SessionManager sessionManager;
-  public void setSessionManager(SessionManager su) {
-	  sessionManager = su;
-  }
+	private long CACHETTL = 300000;
 
-  public String getViewID() {
-	 System.out.println("GOT View " + VIEW_ID);
-    return VIEW_ID;
-  }
+	private SessionManager sessionManager;
 
-  private static Log m_log  = LogFactory.getLog(AccountInfoProducer.class);
-  public void setMessageLocator(MessageLocator messageLocator) {
-    this.messageLocator = messageLocator;
-  }
+	public void setSessionManager(SessionManager su) {
+		sessionManager = su;
+	}
 
-  public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
-    this.userDirectoryService = userDirectoryService;
-  }
+	public String getViewID() {
+		System.out.println("GOT View " + VIEW_ID);
+		return VIEW_ID;
+	}
 
-  
+	private static Log m_log = LogFactory.getLog(AccountInfoProducer.class);
 
-  public void setLocaleGetter(LocaleGetter localegetter) {
-    this.localegetter = localegetter;
-  }
+	public void setMessageLocator(MessageLocator messageLocator) {
+		this.messageLocator = messageLocator;
+	}
 
- 
-  public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) 
-  {
-	  
-	  User user = userDirectoryService.getCurrentUser();
-	  String username = user.getDisplayName();
+	public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
+		this.userDirectoryService = userDirectoryService;
+	}
+
+	public void setLocaleGetter(LocaleGetter localegetter) {
+		this.localegetter = localegetter;
+	}
+
+	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
+
+		User user = userDirectoryService.getCurrentUser();
+		String username = user.getDisplayName();
 		Session session = sessionManager.getCurrentSession();
 		UCTLDAPUser uctUser = null;
-		if (session.getAttribute("ldapUser")==null) {	
-		  uctUser = new UCTLDAPUser(user);
-		  session.setAttribute("ldapUser",uctUser);
+		if (session.getAttribute("ldapUser") == null) {
+			uctUser = new UCTLDAPUser(user);
+			session.setAttribute("ldapUser", uctUser);
 		} else {
 			uctUser = (UCTLDAPUser) session.getAttribute("ldapUser");
 			long cache = new Date().getTime();
 			if (uctUser.getCacheTime().before(new Date(cache - CACHETTL))) {
 				uctUser = new UCTLDAPUser(user);
-				session.setAttribute("ldapUser",uctUser);
+				session.setAttribute("ldapUser", uctUser);
 			}
 		}
-    
-	  UIOutput.make(tofill, "current-username", username);
-	  
-	  
-	  Date passExp = uctUser.getAccountExpiry();
-	  if (passExp != null)
-	  {
-		  DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, localegetter.get());
-		  UIOutput.make(tofill, "ldap-pass-expires", df.format(passExp));
-		  if (uctUser.getAccountIsExpired()==true) {
-			  UIOutput.make(tofill, "ldap-password-good", messageLocator.getMessage("passwd_exp_msg"));
-			  Object[] rep = new Object[]{
-					  (Object) uctUser.getGraceLoginsTotal(),
-					  (Object) uctUser.getGraceLoginsRemaining()};
-			  
-			  UIOutput.make(tofill, "ldap-gracelogins-remaining", messageLocator.getMessage("grace_logins_label", rep));
-		  } 
-	  
-	  }
-	  
-	  Date dob = uctUser.getDOB();
-	  if (dob != null) {
-		  DateFormat monthday = new SimpleDateFormat("MMMMdd");
-		  String dobStr = monthday.format(dob);
-		  String todayStr = monthday.format(new Date());
-		  
-		  if (dob != null && dobStr.equals(todayStr)) {
-			  m_log.info("its this users Birthday!");
-			  UIOutput.make(tofill, "bday");
-		  }
-	  }
 
-	  UILink pLink = UILink.make(tofill, "password_link",messageLocator.getMessage("pwd_selfs_text"), messageLocator.getMessage("pwd_selfs_url"));
-	  //pLink.decorators = new DecoratorList(new UITargetDecorator("_blank"));
-	  
-	  if (user.getType().equals("student") || user.getType().equals("staff")) 	
-		  UIOutput.make(tofill, "seperator");
-	  
-	  if (user.getType().equals("student")) {
-		  
-		  UILink psLink = UILink.make(tofill, "ps_login", messageLocator.getMessage("ps_link_text"), messageLocator.getMessage("ps_student_link"));
-		  
-	  } else if (user.getType().equals("staff")) {
-		  UILink.make(tofill,"ps_login", messageLocator.getMessage("ps_link_text"), messageLocator.getMessage("ps_staff_link"));
-	  }
-	  		
-	  
-  }
+		UIOutput.make(tofill, "current-username", username);
 
-  public List reportNavigationCases() {
-    List togo = new ArrayList(); // Always navigate back to this view.
-    togo.add(new NavigationCase(null, new SimpleViewParameters(VIEW_ID)));
-    return togo;
-  }
+		Date passExp = uctUser.getAccountExpiry();
+		if (passExp != null) {
+			DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, localegetter.get());
+			UIOutput.make(tofill, "ldap-pass-expires", df.format(passExp));
+			if (uctUser.getAccountIsExpired() == true) {
+				UIOutput.make(tofill, "ldap-password-good", messageLocator.getMessage("passwd_exp_msg"));
+				Object[] rep = new Object[] { (Object) uctUser.getGraceLoginsTotal(),
+						(Object) uctUser.getGraceLoginsRemaining() };
+
+				UIOutput.make(tofill, "ldap-gracelogins-remaining",
+						messageLocator.getMessage("grace_logins_label", rep));
+			}
+
+		}
+
+		Date dob = uctUser.getDOB();
+		if (dob != null) {
+			DateFormat monthday = new SimpleDateFormat("MMMMdd");
+			String dobStr = monthday.format(dob);
+			String todayStr = monthday.format(new Date());
+
+			if (dob != null && dobStr.equals(todayStr)) {
+				m_log.info("its this users Birthday!");
+				UIOutput.make(tofill, "bday");
+			}
+		}
+
+		UILink pLink = UILink.make(tofill, "password_link", messageLocator.getMessage("pwd_selfs_text"),
+				messageLocator.getMessage("pwd_selfs_url"));
+		// pLink.decorators = new DecoratorList(new
+		// UITargetDecorator("_blank"));
+
+		if (user.getType().equals("student") || user.getType().equals("staff"))
+			UIOutput.make(tofill, "seperator");
+
+		if (user.getType().equals("student")) {
+
+			UILink psLink = UILink.make(tofill, "ps_login", messageLocator.getMessage("ps_link_text"),
+					messageLocator.getMessage("ps_student_link"));
+
+		} else if (user.getType().equals("staff")) {
+			UILink.make(tofill, "ps_login", messageLocator.getMessage("ps_link_text"),
+					messageLocator.getMessage("ps_staff_link"));
+		}
+
+	}
+
+	public List<NavigationCase> reportNavigationCases() {
+		// Always navigate back to this view.
+		List<NavigationCase> togo = new ArrayList<NavigationCase>(); 
+		togo.add(new NavigationCase(null, new SimpleViewParameters(VIEW_ID)));
+		return togo;
+	}
 
 }
